0bf9f26828d1d0121e1e302c5a0537ba7fda0327
Kevin Luke <kkevinluke@gmail.com>
2024-03-23 21:43:27 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import time  # Import the time module
 
 # Initialize a counter for widget keys
 widget_counter = 0
